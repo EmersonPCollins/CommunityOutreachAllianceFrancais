9a7759958522d1ed0fdf45d2bd9a3f12a59385c4
Sam Jawich <ws852206@dal.ca>
2020-02-13 12:48:27 -0400

    Added imageUrl property to Event class

--- app/src/main/java/com/csci/afevents/entities/Event.java
+++ app/src/main/java/com/csci/afevents/entities/Event.java
@@ -4,12 +4,14 @@ public class Event {
     private String eventId;
     private String eventName;
     private String description;
+    private String imageUrl;
     private long date;
 
-    public Event(String eventId, String eventName, String description, long date) {
+    public Event(String eventId, String eventName, String description, String imageUrl, long date) {
         this.eventId = eventId;
         this.eventName = eventName;
         this.description = description;
+        this.imageUrl = imageUrl;
         this.date = date;
     }
 }
--- app/src/main/java/com/csci/afevents/impl/DummyEventRetriever.java
+++ app/src/main/java/com/csci/afevents/impl/DummyEventRetriever.java
@@ -11,11 +11,11 @@ public class DummyEventRetriever implements EventRetriever {
     @Override
     public List<Event> getEvents() {
         List<Event> list= new ArrayList<>();
-        list.add(new Event("#1","Wine Tasting","Come drink some wine and eat cheese",20200217));
-        list.add(new Event("#2","Home-coming","Coming back home",20200221));
-        list.add(new Event("#3","First day of School","Come drink some wine and eat cheese",20200217));
-        list.add(new Event("#4","Event 4","Come drink some wine and eat cheese",20200312));
-        list.add(new Event("#5","Event 5","Come drink some wine and eat cheese",20200417));
+        list.add(new Event("#1","Wine Tasting","Come drink some wine and eat cheese","https://via.placeholder.com/200",20200217));
+        list.add(new Event("#2","Home-coming","Coming back home","https://via.placeholder.com/200",20200221));
+        list.add(new Event("#3","First day of School","Come drink some wine and eat cheese","https://via.placeholder.com/200",20200217));
+        list.add(new Event("#4","Event 4","Come drink some wine and eat cheese","https://via.placeholder.com/200",20200312));
+        list.add(new Event("#5","Event 5","Come drink some wine and eat cheese","https://via.placeholder.com/200",20200417));
         return list;
     }
 }
