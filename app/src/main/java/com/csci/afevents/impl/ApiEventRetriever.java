bc20687906088c3ee9b2b8441bf499db613eca52
ecollins <em931418@dal.ca>
2020-02-27 12:53:24 -0400

    API start

--- app/src/main/java/com/csci/afevents/impl/ApiEventRetriever.java
+++ app/src/main/java/com/csci/afevents/impl/ApiEventRetriever.java
@@ -1,5 +1,11 @@
 package com.csci.afevents.impl;
 
+import ;
+import android.app.DownloadManager;
+import android.widget.TextView;
+import android.util.JsonReader;
+
+import com.csci.afevents.R;
 import com.csci.afevents.api.EventRetriever;
 import com.csci.afevents.entities.Event;
 
@@ -8,6 +14,11 @@ import java.util.List;
 public class ApiEventRetriever implements EventRetriever {
     @Override
     public List<Event> getEvents() {
-        return null;
+
+        final TextView textView = (TextView) findViewById(R.id.text);
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="http://dummy.restapiexample.com/api/v1/employees";
+
+        StringRequest stringRequest = new StringRequest(DownloadManager.Request.Method.POST, url, new Response.Listener<String>();
     }
 }
