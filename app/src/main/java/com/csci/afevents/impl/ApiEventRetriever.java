ab04321dffb283ae9eb27937a6e6d90e87b5ea03
Sam Jawich <ws852206@dal.ca>
2020-02-27 12:25:33 -0400

    Stub implementation of API Event retriever

---
+++ app/src/main/java/com/csci/afevents/impl/ApiEventRetriever.java
@@ -0,0 +1,13 @@
+package com.csci.afevents.impl;
+
+import com.csci.afevents.api.EventRetriever;
+import com.csci.afevents.entities.Event;
+
+import java.util.List;
+
+public class ApiEventRetriever implements EventRetriever {
+    @Override
+    public List<Event> getEvents() {
+        return null;
+    }
+}
