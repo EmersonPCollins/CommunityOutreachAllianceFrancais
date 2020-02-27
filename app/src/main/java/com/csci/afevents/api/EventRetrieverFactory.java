75146f3eb7e0c1a6f306af80f466d40ee4fa094a
ecollins <em931418@dal.ca>
2020-02-27 11:48:15 -0400

    Connection implementation #12

--- app/src/main/java/com/csci/afevents/api/EventRetrieverFactory.java
+++ app/src/main/java/com/csci/afevents/api/EventRetrieverFactory.java
@@ -1,13 +1,25 @@
 package com.csci.afevents.api;
-
 import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 
 import com.csci.afevents.impl.DummyEventRetriever;
 
 public class EventRetrieverFactory {
 
     public static EventRetriever getInstance(Context context) {
-        //TODO: Return API or DB retriever when available
         return new DummyEventRetriever();
     }
+
+    private static boolean isConnected(Context context) {
+        boolean connected = false;
+        //ensure we are connected to the internet
+        ConnectivityManager connectivityManager = (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if(connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState() == NetworkInfo.State.CONNECTED ||
+                connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState() == NetworkInfo.State.CONNECTED) {
+            connected = true;
+        }
+        return connected;
+    }
+
 }
