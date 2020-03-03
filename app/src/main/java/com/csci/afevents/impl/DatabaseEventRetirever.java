7d528345076c2dd73cdfdf36b578ed019d58b562
patelnisarg <npatel@dal.ca>
2020-03-03 12:05:46 -0400

    Made a new java file named DatabaseEventRetriever.java

---
+++ app/src/main/java/com/csci/afevents/impl/DatabaseEventRetirever.java
@@ -0,0 +1,15 @@
+package com.csci.afevents.impl;
+
+import com.csci.afevents.api.EventRetriever;
+import com.csci.afevents.entities.Event;
+
+import java.util.List;
+
+public class DatabaseEventRetirever implements EventRetriever {
+
+    @Override
+    public List<Event> getEvents() {
+        return null;
+    }
+
+}
