371662ce3c9937dd3ef695751ccc01b198c17c09
Thomas Sorrell <th455539@dal.ca>
2020-03-12 13:09:22 -0300

    Merge branch 'LoadingPannel' of C:\Users\Thomus Sorrell\csci2691\philippefullsack_alliancefrancaise with conflicts.

--- app/src/main/java/com/csci/afevents/MainActivity.java
+++ app/src/main/java/com/csci/afevents/MainActivity.java
@@ -10,6 +10,8 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import static android.view.View.GONE;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -25,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
+        findViewById(R.id.loadingPanel).setVisibility(GONE);
     }
 
 }
